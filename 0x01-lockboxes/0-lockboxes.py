86000986a9094eb3a9e452a88b1afcfcfe229195
Túlio Benedito Nhantumbo <80028633+beneditotulio@users.noreply.github.com>
2024-09-05 10:58:43 +0200

    feat: Refactor canUnlockAll function in lockboxes module
    The code changes refactor the `canUnlockAll` function in the lockboxes module. The function now uses a more efficient algorithm to check if all the boxes can be unlocked. This commit aligns with the recent repository commits that focus on improving code quality and performance.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""
-Lockboxes module
-"""
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
-    """Method that verifies if all
-    Boxes can be unlocked
-        - Return:
-            - true -> when all can be unlock
-            - false -> when we cant
-    """
-    size = len(boxes)
-    print(boxes[0])
-    goNext(boxes[0], boxes)
-    return True
-
-def goNext(box, boxes):
-    """test"""
-    if len(box) == 0:
-        return 0
-    next_box = boxes[box.pop(0)]
-    # next_box = boxes[box[0]]
-    # print(box)
-    print(next_box)
-    goNext(next_box, boxes)
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
